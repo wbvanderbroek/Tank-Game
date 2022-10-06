51c6772cd67fa07b2a97d457021c7bf8ab3b2cb8
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2022-10-06 13:24:18 +0200

    turnbased system werkt

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -6,20 +6,49 @@ public class TurnManager : MonoBehaviour
 {
     [SerializeField] GameObject Tank1;
     [SerializeField] GameObject Tank2;
+    [SerializeField] GameObject Bullet;
+    private int TotalAmount = 0;
+
+    private void Update()
+    {
+        TotalAmount = GameObject.FindGameObjectsWithTag("Bullet").Length;
+    }
     public void InvokeTank1()
     {
-        Invoke("EnableTank1", 2.5f);
+        Invoke("EnableTank1", 1f);
     }
     public void InvokeTank2()
     {
-        Invoke("EnableTank2", 2.5f);
+        Invoke("EnableTank2", 1f);
     }
     private void EnableTank1()
     {
-        Tank1.GetComponent<TankController>().enabled = true;
+        if (TotalAmount == 0)
+        {
+            Tank1.GetComponent<TankController>().enabled = true;
+        }
+        else
+        {
+            Invoke("InvokeTank11", 0.5f);
+        }
     }
     private void EnableTank2()
     {
-        Tank2.GetComponent<TankController2>().enabled = true;
+        if (TotalAmount == 0)
+        {
+            Tank2.GetComponent<TankController2>().enabled = true;
+        }
+        else
+        {
+            Invoke("InvokeTank22", 0.5f);  
+        }
+    }
+    private void InvokeTank11 ()
+    {
+        EnableTank1 ();
+    }
+    private void InvokeTank22()
+    {
+        EnableTank2();
     }
 }
