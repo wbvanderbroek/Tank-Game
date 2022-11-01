022d002a1b20c0dd6b937328e96df6056db74710
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2022-11-01 14:05:07 +0100

    pivots fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] GameObject tank1;
-    [SerializeField] GameObject tank2;
+    [SerializeField] Transform tank1;
+    [SerializeField] Transform tank2;
     [SerializeField] private float trackingSpeed = 12f;
     [SerializeField] TankController controller1;
     [SerializeField] TankController controller2;
@@ -13,15 +13,15 @@ public class CameraController : MonoBehaviour
     private Vector3 target;
     void Update()
     {
-        if (controller1.isActiveAndEnabled == true)
+        if (controller1.isPlayerTurn == true)
         {
-            target = new Vector3(tank1.transform.position.x +5, tank1.transform.position.y, -10);
+            target = new Vector3(tank1.transform.position.x +8, tank1.transform.position.y +4, -10);
             transform.rotation = Quaternion.Euler(0, 0, 0);
             transform.position = Vector3.MoveTowards(transform.position, target, trackingSpeed * Time.deltaTime);
         }
-        if (controller2.isActiveAndEnabled == true)
+        if (controller2.isPlayerTurn == true)
         {
-            target = new Vector3(tank2.transform.position.x -18.5f, tank2.transform.position.y, -10);
+            target = new Vector3(tank2.transform.position.x -8, tank2.transform.position.y +4, -10);
             transform.rotation = Quaternion.Euler(0, 0, 0);
             transform.position = Vector3.MoveTowards(transform.position, target, trackingSpeed * Time.deltaTime);
         }
